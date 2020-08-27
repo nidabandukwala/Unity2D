cb66980d814a7c183e3b3a146efb66984e89f336
nidabandukwala <59574522+nidabandukwala@users.noreply.github.com>
2020-08-27 14:31:09 -0400

    Adding Laser Defender project folder

---
+++ Laser Defender/HealthDisplay.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using TMPro;
+
+public class HealthDisplay : MonoBehaviour
+{
+
+    TextMeshProUGUI healthText;
+    Player player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthText = GetComponent<TextMeshProUGUI>();
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthText.text = player.GetHealth().ToString();
+    }
+}
---
+++ Laser Defender/Level.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Level : MonoBehaviour
+{
+
+    [SerializeField] float delayInSeconds = 2f;
+
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+        FindObjectOfType<GameSession>().ResetGame();
+
+    }
+
+    public void LoadGameOver()
+    {
+        StartCoroutine(WaitAndLoad());
+        SceneManager.LoadScene("Game Over");
+    }
+
+    IEnumerator WaitAndLoad()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
