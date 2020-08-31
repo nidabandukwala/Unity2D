9cf7b7a18efaec04322ab41b208ff81785ef3dac
nidabandukwala <59574522+nidabandukwala@users.noreply.github.com>
2020-08-31 11:34:17 -0400

    Adding Number Wizard folder

---
+++ Number Wizard/NumberWizard.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using TMPro;
+
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int maxguess;
+    [SerializeField] int minguess;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        
+    }
+
+    public void OnPressHigher()
+    {
+        minguess = guess + 1;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        maxguess = guess - 1;
+        NextGuess();
+    }
+
+    // Update is called once per frame
+    
+
+    void NextGuess()
+    {
+        guess = Random.Range(minguess,maxguess + 1);
+        guessText.text = guess.ToString();
+
+    }
+
+    
+
+
+    
+
+    
+}
+
